cb4a9fb89e9efe37d1938b66c6a3d2d3e4fe4437
Coffi <juanmanez.dev@gmail.com>
2019-10-07 14:02:54 +0200

    Yarn error

--- misc/preinstall.js
+++ misc/preinstall.js
@@ -1,13 +1,13 @@
-if (process.env.npm_execpath.indexOf('yarn') === -1) {
-  throw new Error(`
+// if (process.env.npm_execpath.indexOf('yarn') === -1) {
+//   throw new Error(`
   
-  ###################################################
-  #                                                 #
-  #  Please use Yarn > 1.3 to install dependencies  #
-  #                                                 #
-  #  1. Install yarn (https://yarnpkg.com/)         #
-  #  2. Run 'yarn' instead of 'npm install'         #
-  #                                                 #
-  ###################################################
-  `);
-}
+//   ###################################################
+//   #                                                 #
+//   #  Please use Yarn > 1.3 to install dependencies  #
+//   #                                                 #
+//   #  1. Install yarn (https://yarnpkg.com/)         #
+//   #  2. Run 'yarn' instead of 'npm install'         #
+//   #                                                 #
+//   ###################################################
+//   `);
+// }
